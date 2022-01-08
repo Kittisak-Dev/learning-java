be0d41cd1f10f2abe233823d372b873f497b5046
Kittisak S <97340824+Kittisak-Dev@users.noreply.github.com>
2022-01-08 22:59:14 +0700

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class variable {
+    public static void main(String[] args) {
+        Date now = new Date();
+        int data = 50;
+        System.out.println("Variable! " + data);
+        System.out.println("time: " + now);
+    }
+}
